5e9cd701c828a9c7f68d3c489fab544344c69e35
Birnadin Erick <45619033+BirnadinErick@users.noreply.github.com>
2023-08-14 09:42:14 +0530

    feat: parse the img links b4 move

--- mv-files.py
+++ mv-files.py
@@ -1,22 +1,41 @@
 import sys
 import os
-import shutil
+import re
+
+# Get params from cmdline args
+MANIFEST = sys.argv[1]
+DESTINATION = sys.argv[2] or "files"
+
+def parse_img_links(c, r, s):
+    """
+    * Takes MD content `c` and substitutes patterns matching `r` with `s`
+    param c: str; content of MD file to be parsed
+    param r: rstr; regex pattern to be substituted
+    param s: str; substitute pattern
+    """
+    result = re.sub(r, s, c, 0, re.MULTILINE)
+    return result
 
 # Create the "files" directory if it doesn't exist
-os.makedirs("files", exist_ok=True)
+os.makedirs(DESTINATION, exist_ok=True)
 
 # Get the filenames to be moved
-MANIFEST = sys.argv[1]
 with open(MANIFEST, "r") as m:
-    d_files = [f for f in m.read().split(",")]
+    files_to_move = [f.replace("\n", "").rstrip().lstrip() for f in m.read().split(",")]
 
-# Get a list of all files in the current working directory
-files_to_move = [filename for filename in d_files if os.path.isfile(filename)]
+REGEX = r"!\[([\d\w\s]*)\]\(([\d\w:\/.*-]*)\s[\w\"=]*\)"
+SUBSTITUTE = r"![\1](\2)"
 
-# Move each file to the "files" directory
-for filename in files_to_move:
-    try:
-        shutil.move(filename, os.path.join("files", filename))
-        print(f"Moved {filename} to files/")
-    except Exception as e:
-        print(f"Error moving {filename}: {str(e)}")
+# Create map of files
+files = dict()
+for f in files_to_move:
+    with open(f, "r") as h_f:
+        files[f] = h_f.read()
+
+# Parse the contents
+files = {k: parse_img_links(v, REGEX, SUBSTITUTE) for k, v in files.items()}
+
+# Write it to the destination
+for f, c in files.items():
+    with open(os.path.join(DESTINATION, f), "x") as h_f:
+        h_f.write(c)
